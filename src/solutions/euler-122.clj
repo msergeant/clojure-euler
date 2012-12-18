3f8e6424ab7eeff3199e4cbfdf232be6b5647a1f
Mike Sergeant <sergeant.mj@gmail.com>
2012-12-17 23:41:09 -0500

    Solved 122 (1700 ms)

--- src/solutions/euler-122.clj
+++ src/solutions/euler-122.clj
@@ -1,6 +1,30 @@
 (defn euler-122 [limit]
-  (let [multis (ref (zipmap (range 1 (+ 1 limit)) (repeat 99)))]
-    ;;Actual calculation will go in here
-    (reduce + (vals @multis))
+  (let [multis (ref (vec (cons 0 (take (+ 1 200) (repeat 99)))))]
+    ((fn node-calculation
+      [path]
+      (if (or (< limit (last path)) (< (nth @multis (last path)) (dec (count path))))
+              nil
+              ((fn [] 
+                 (dosync (ref-set multis (assoc @multis (last path) (dec (count path)))))
+                 (loop
+                   [iterator (count path)
+                    loop-path path]
+                   (if (>= 0 iterator)
+                     nil
+                     (do
+                       (node-calculation
+                         (conj loop-path (+ (last loop-path) (nth loop-path (dec iterator)))))
+                       (recur
+                         (dec iterator) loop-path)
+                         )
+                       )
+                     )
+                   ))
+                )
+        ) [1])
+    (reduce + (take (+ 1 limit) @multis))
+    )
   )
-)
+
+;; Solution: 1582
+;; Time: 1700 ms
