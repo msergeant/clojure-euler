f87f5e306b9ea1b31fc812e703aa8003e4a71e45
Mike Sergeant <sergeant.mj@gmail.com>
2012-12-20 23:33:55 -0500

    Finding the min for the example.
    Still need to display sample string and change vals for n=7

--- src/solutions/euler-103.clj
+++ src/solutions/euler-103.clj
@@ -6,6 +6,15 @@
                                          itm)) coll))
   )
 
+(defn bit-count
+  ([x]
+   (bit-count x 0))
+  ([x c]
+    (if (zero? x)
+      c
+     (recur (bit-and x (dec x)) (inc c))))
+ )
+
 (defn test-cond-i [coll]
   (reduce (fn [x y] (and x y)) 
           (for [ x (range 1 (exp 2 (count coll)))
@@ -17,7 +26,30 @@
   )
 
 (defn test-cond-ii [coll]
+  (reduce (fn [x y] (and x y)) 
+          (for [ x (range 1 (exp 2 (count coll)))
+                 y (range 1 (exp 2 (count coll)))
+                 :when (and (> y x) (= 0 (bit-and x y)) (not= (bit-count x) (bit-count y)))]
+            (if (> (bit-count x) (bit-count y))
+              (> (sum-subset coll x) (sum-subset coll y))
+              (< (sum-subset coll x) (sum-subset coll y))
+            )
+    )
   )
+)
 
-(defn euler-103 [guess]
+;;{11, 17, 20, 22, 23, 24}
+(defn euler-103 []
+  (reduce min (for
+    [a (range 9 12)
+     b (range 15 19)
+     c (range 18 22)
+     d (range 20 24)
+     e (range 21 25)
+     f (range 22 26)]
+    (let [x [a b c d e f]]
+    (if (and (test-cond-i x) (test-cond-ii x))
+      (reduce + x)
+      99999999))
+          ))
   )
