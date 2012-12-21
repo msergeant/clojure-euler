bd94d0b687fa99a7c8ef6484b8ef4d5c6825f16a
Mike Sergeant <sergeant.mj@gmail.com>
2012-12-21 13:57:37 -0500

    Solved 103. 53 seconds

--- src/solutions/euler-103.clj
+++ src/solutions/euler-103.clj
@@ -39,18 +39,19 @@
 )
 
 ;;{11, 17, 20, 22, 23, 24}
-;;{22, 33, 39, 42, 44, 45, 46}
+;;{20, 31, 38, 39, 40, 42, 45}
 (defn euler-103 []
-  (let [candidates (for
-    [a (range 20 24)
-     b (range 31 35)
-     c (range 37 41)
-     d (range 40 44)
-     e (range 42 46)
-     f (range 43 47)
-     g (range 44 48)
+  (let [guess [20, 31, 38, 39, 40, 42, 45]
+        candidates (for
+    [a (range 18 22)
+     b (range 29 33)
+     c (range 36 40)
+     d (range 37 41)
+     e (range 38 42)
+     f (range 40 44)
+     g (range 43 48)
      :when (and (< a b) (< b c) (< c d) (< d e) (< e f) (< f g))]
-    (let [x [a b c d e f]]
+    (let [x [a b c d e f g]]
     (if (and (test-cond-i x) (test-cond-ii x))
       x
       nil))
@@ -70,3 +71,7 @@
     )
   )
 )
+
+
+;; Solution: 20313839404245
+;; Time: 53 seconds
