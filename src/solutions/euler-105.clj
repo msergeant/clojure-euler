f9587acbe4947e905f568e09fcf0b55ddf0f00f5
Mike Sergeant <sergeant.mj@gmail.com>
2012-12-21 22:48:11 -0500

    Solved 105. 185 seconds

---
+++ src/solutions/euler-105.clj
@@ -0,0 +1,24 @@
+(load-file "src/solutions/subset-functions.clj")
+(use 'clojure.java.io)
+(use '[clojure.string :only (split)])
+
+(defn process-file [file-name line-func line-acc]
+  (with-open [rdr (reader file-name)]
+    (reduce line-func line-acc (line-seq rdr)))
+  )
+
+(defn process-line [acc line]
+  (let [tst-list (map #(Integer/parseInt %) (vec (split line #",")))]
+   (if (and (test-cond-i tst-list) (test-cond-ii tst-list))
+     (+ acc (reduce + tst-list))
+     acc
+    )
+    )
+  )
+
+(defn euler-105 []
+  (process-file "src/solutions/sets.txt" process-line 0)
+  )
+
+;; Solution: 185484
+;; Time: 185 seconds
