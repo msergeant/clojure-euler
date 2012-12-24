49cd124544506c35a49f5326ae978a6e10831bba
Mike Sergeant <sergeant.mj@gmail.com>
2012-12-24 13:42:48 -0500

    Solved 107. 148 ms

--- src/solutions/euler-107.clj
+++ src/solutions/euler-107.clj
@@ -16,12 +16,45 @@
 
 (defn zero-if-nil [x] (if (nil? x) 0 x))
 (defn add-nils [x, y] (+ (zero-if-nil x) (zero-if-nil y)))
+(defn shortest-next [covered not-covered graph]
+  (let [edges (for [begin covered
+                    end not-covered
+                    :when (not (nil? (nth (nth graph end) begin)))]
+                (vector [begin end] (nth (nth graph end) begin)))]
+        (loop [minimum (last (first edges))
+               min-edge (first (last edges))
+               index 0]
+          (if (>= index (count edges))
+            min-edge
+            (if (< minimum (last (nth edges index)))
+              (recur minimum min-edge (inc index))
+              (recur (last (nth edges index)) (first (nth edges index)) (inc index))
+              )
+            )
+          )
+    )
+  )
 
-(defn euler-107 []
-  (let [the-matrix (process-file "src/solutions/network_example.txt" process-line [])
+(defn euler-107 
+  "Solves euler projet problem 107 using Prim's algorithm"
+  []
+  (let [the-matrix (process-file "src/solutions/network.txt" process-line [])
         original-weight (/ (reduce + (map #(reduce add-nils %) the-matrix)) 2)
         width (count (nth the-matrix 0))]
-    (println original-weight)
-    (println width)
+    (- original-weight
+      (loop [covered #{0}
+             not-covered (set (range 1 width))
+             total 0 
+             ]
+        (if (empty? not-covered)
+          total
+          (let [next-edge (shortest-next covered not-covered the-matrix)
+                now-covered (last next-edge)]
+            (recur (conj covered now-covered) 
+                   (disj not-covered now-covered) 
+                   (+ total (nth (nth the-matrix (last next-edge)) (first next-edge))))
+            )
+          )
+        ))
     )
   )
