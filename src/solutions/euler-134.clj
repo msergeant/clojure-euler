be6b39dbebe8ea3cdf672d76014e4b31c2bef395
Mike Sergeant <sergeant.mj@gmail.com>
2013-01-12 16:03:33 -0500

    Solved 134. 128 seconds.

--- src/solutions/euler-134.clj
+++ src/solutions/euler-134.clj
@@ -1,11 +1,28 @@
 (load-file "src/solutions/common-functions.clj")
 
-(def consecutive-primes (for [x (range 6 1000000) :when (prime? x)] x)
+(def consecutive-primes (for [x (range 5 1000004) :when (prime? x)] x)
   )
+
+;; Extended Euclidean Algorithm
+;; http://en.wikipedia.org/wiki/Extended_Euclidean_algorithm
+(defn ext-gcd [a0 b0]
+  (loop [x 0
+         y 1
+         lastx 1
+         lasty 0
+         q (int (/ a0 b0))
+         a b0
+         b (mod a0 b0)]
+    (if (= b 0)
+      x
+      (recur (- lastx (* q x)) (- lasty (* q y)) x y (int (/ a b)) b (mod a b))
+      )
+    )
+  )
+
 (defn S [x y]
   (let [p10 (int (pow 10 (int (+ 1 (log10 x)))))]
-    (reduce (fn [a b] (if (= x (mod (* y b) p10)) (* a (* y b)) (* a 1))) 1 (range 1 p10)
-            )
+    (+ x (* p10 (mod (* (- y x) (ext-gcd p10 y)) y)))
     )
   )
 (defn euler-134 []
@@ -13,3 +30,6 @@
     (S (nth consecutive-primes (- x 1)) (nth consecutive-primes x)))
           )
   )
+
+;; Solution: 18613426663617118
+;; Time: 128 seconds
