67e1ab230405814c99e421cbaf60b595fba7b4a1
Mike Sergeant <sergeant.mj@gmail.com>
2013-01-11 23:06:41 -0500

    Load math library

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject clojure-euler "1.0.0-SNAPSHOT"
   :description "Solutions to ProjectEuler problems"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
