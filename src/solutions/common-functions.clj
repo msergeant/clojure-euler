a8a82d8891056f774212dfb7899bce3935e15948
Mike Sergeant <sergeant.mj@gmail.com>
2013-01-11 23:07:35 -0500

    Add some functions to common file

--- src/solutions/common-functions.clj
+++ src/solutions/common-functions.clj
@@ -1,6 +1,18 @@
-(defn exp [x n]
-  (reduce * (repeat n x))
-  )
+(use '[clojure.contrib.generic.math-functions])
+
+(defn log10 [n] (/ (clojure.contrib.generic.math-functions/log n) (clojure.contrib.generic.math-functions/log 10)))
 
 (defn remove-index [coll n] (vec (concat (subvec coll 0 n) (subvec coll (+ 1 n)))))
 
+(defn square [n] (* n n))
+
+(defn divides? [a b]
+          (= (mod b a) 0))
+(defn find-divisor [n test-divisor]
+          (cond (> (square test-divisor) n) n
+                (divides? test-divisor n) test-divisor
+                :else (find-divisor n (+ test-divisor 1))))
+(defn smallest-divisor [n]
+          (find-divisor n 2))
+(defn prime? [n]
+          (= n (smallest-divisor n)))
